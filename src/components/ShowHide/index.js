16610d0658e7b69bd87788dbc23d5a2d5b7a5dd8
ppunem <prathyusha2094@gmail.com>
2023-08-08 15:56:46 +0000

    final

--- src/components/ShowHide/index.js
+++ src/components/ShowHide/index.js
@@ -2,71 +2,72 @@ import {Component} from 'react'
 import './index.css'
 
 class ShowHide extends Component {
-    state={clicked:false}
+  state = {firstClicked: false, lastClicked: false}
 
-    displayFirstname=()=>{
-       if (clicked===false){
-           this.setState(prevState=>(
-               {clicked:true}
-           ))
-       }else{
-           this.setState(prevState=>{
-               {clicked:false}
-           })
-       }
+  displayFirstname = () => {
+    const {firstClicked} = this.state
+    if (firstClicked === false) {
+      this.setState({firstClicked: true})
+    } else {
+      this.setState({
+        firstClicked: false,
+      })
     }
+  }
 
-        displayLastname=()=>{
-       if (clicked===false){
-           this.setState(prevState=>(
-               {clicked:true}
-           ))
-       }else{
-           this.setState(prevState=>{
-               {clicked:false}
-           })
-       }
+  displayLastname = () => {
+    const {lastClicked} = this.state
+    if (lastClicked === false) {
+      this.setState({lastClicked: true})
+    } else {
+      this.setState({
+        lastClicked: false,
+      })
     }
+  }
 
-    
-
-    
   render() {
-      const {clicked}=this.state
-      let firstName;
-      if (clicked===true){
-         firstName=<div className="first-last-name-card">Joe</div>
-      }else{
-          firstName=null
-      }
+    const {firstClicked, lastClicked} = this.state
+    let firstName
+    if (firstClicked === true) {
+      firstName = <div className="first-last-name-card">Joe</div>
+    } else {
+      firstName = <div className="first-last-name-card-null">Joe</div>
+    }
 
-      let lastName;
-      if (clicked===true){
-         lastName=<div className="first-last-name-card">Jonas</div>
-      }else{
-          lastName=null
-      }
+    let lastName
+    if (lastClicked === true) {
+      lastName = <div className="first-last-name-card">Jonas</div>
+    } else {
+      lastName = <div className="first-last-name-card-null">Joe</div>
+    }
     return (
       <div className="bg">
         <h1 className="main-heading">Show/Hide</h1>
         <div className="card">
           <div className="first-last-name">
             <div className="btn-align">
-              <button className="first-last-name-button" type="button" onClick={this.displayFirstname}>
+              <button
+                className="first-last-name-button"
+                type="button"
+                onClick={this.displayFirstname}
+              >
                 Show/Hide Firstname
               </button>
+              {firstName}
             </div>
-            {this.firstName}
-            
+          </div>
           <div className="first-last-name">
             <div className="btn-align">
-              <button className="first-last-name-button" type="button" onClick={this.displayLastname}>
+              <button
+                className="first-last-name-button"
+                type="button"
+                onClick={this.displayLastname}
+              >
                 Show/Hide Lastname
               </button>
+              {lastName}
             </div>
-            {this.lastName}
-
-            <div className="first-last-name-card">Jonas</div>
           </div>
         </div>
       </div>
